51cdb598ef02cad654e0fd6334a969addc5f7887
baldx <mr.ahmad.m2006@gmail.com>
2024-02-15 16:48:28 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,37 @@ function App() {
           </div>
         </div>
       </main>
+
+      <footer>
+      <div className="left-footer">
+        <img src="./src/assets/JewelryLogo.png" alt="Logo" className="logo" />
+        <div className="logo-name">Divine Desires</div>
+      </div>
+      <div className="credit-right">
+        <div className="credit-contributors">Contributors:</div>
+        <div className="link-section">
+          <div className="link-name">
+            <span>Ahmad</span>
+            <span>Hugo</span>
+            <span>Alvin</span>
+          </div>
+          <div className="link-container">
+            <a target="_blank" href="https://www.instagram.com/ahmad.nocap/">
+              <img src="./src/assets/black-instagram-transparent-logo-10671.svg" alt="" className="ig" />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/hugowilinski/">
+              <img src="./src/assets/black-instagram-transparent-logo-10671.svg" alt="" className="ig" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+            >
+              <img src="./src/assets/black-instagram-transparent-logo-10671.svg" alt="" className="ig" />
+            </a>
+          </div>
+        </div>
+      </div>
+      </footer>
     </>
   )
 }
