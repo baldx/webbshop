3ce782ebe077df2fd0118e6e11e5513e96f447bb
baldx <mr.ahmad.m2006@gmail.com>
2024-02-15 16:08:24 +0100

    add main

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,22 @@ function App() {
         <NavBarItems />
         <SearchBar />
       </nav>
+      <main>
+        <div className="top">
+          <img src="./src/assets/fontImage.jpg" alt="" className='front-image'/>
+          <div className="info">
+            <h1>Utility Accessories</h1>
+            <button>Shop now</button>
+          </div>
+        </div>
+        <div className="top">
+          <img src="./src/assets/bottomImage.jpg" alt="" className='front-image'/>
+          <div className="info">
+            <h1>New in</h1>
+            <button>Shop now</button>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
